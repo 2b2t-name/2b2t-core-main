584fea4864e4b8b6acae972e3e8da89203139103
LachCrafter <lachcrafteryt@gmail.com>
2024-07-18 12:59:24 +0200

    Updated mainclassname

--- src/main/java/eu/felicianware/core/Main.java
+++ src/main/java/eu/felicianware/core/Main.java
@@ -2,7 +2,7 @@ package eu.felicianware.core;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class Core extends JavaPlugin {
+public final class Main extends JavaPlugin {
 
     @Override
     public void onEnable() {
