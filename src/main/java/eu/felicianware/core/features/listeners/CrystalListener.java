759594cc79ea972a069669f7c581caade76bd421
LachCrafter <lachcrafteryt@gmail.com>
2024-07-22 15:49:59 +0200

    rewrite crystallistener

--- src/main/java/eu/felicianware/core/features/listeners/CrystalListener.java
+++ src/main/java/eu/felicianware/core/features/listeners/CrystalListener.java
@@ -1,80 +1,58 @@
 package eu.felicianware.core.features.listeners;
 
-import eu.felicianware.core.managers.ConfigManager;
-import io.papermc.paper.threadedregions.scheduler.RegionScheduler;
-import org.bukkit.Location;
-import org.bukkit.Material;
-import org.bukkit.World;
-import org.bukkit.entity.EnderCrystal;
-import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
-import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
-import org.bukkit.event.block.Action;
-import org.bukkit.event.entity.EntityDamageByEntityEvent;
-import org.bukkit.event.player.PlayerInteractEvent;
-import org.bukkit.inventory.ItemStack;
-import org.bukkit.plugin.Plugin;
-import org.jetbrains.annotations.NotNull;
+import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.block.Block;
+import org.bukkit.Material;
+import org.bukkit.entity.EnderCrystal;
+import org.bukkit.Bukkit;
+import org.bukkit.scheduler.BukkitRunnable;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.UUID;
 
-public class CrystalListener implements Listener {
-    private final Plugin plugin;
-    private final Set<UUID> placeCooldowns = new HashSet<>();
+public class CrystalListener extends JavaPlugin implements Listener {
 
-    private final long PLACE_DELAY_TICKS;
-    private final long EXPLOSION_DELAY_TICKS;
-    private final float EXPLOSION_POWER;
+    private final Map<UUID, Integer> crystalSpawnTicks = new HashMap<>();
 
-    public CrystalListener(Plugin plugin, ConfigManager configManager) {
-        this.plugin = plugin;
-        // Configurable delays and power
-        long PLACE_DELAY_MILLIS = configManager.getPlaceDelayMillis();
-        this.PLACE_DELAY_TICKS = PLACE_DELAY_MILLIS / 50;
-        this.EXPLOSION_DELAY_TICKS = configManager.getExplosionDelayTicks();
-        this.EXPLOSION_POWER = configManager.getExplosionPower();
-    }
-
-    @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
-    public void onCrystalPlace(@NotNull PlayerInteractEvent event) {
-        if (event.getAction() != Action.RIGHT_CLICK_BLOCK) return;
-
-        final ItemStack interactItem = event.getItem();
-        if (interactItem == null || interactItem.getType() != Material.END_CRYSTAL) return;
-
-        final Player player = event.getPlayer();
-        final UUID playerUUID = player.getUniqueId();
-
-        if (placeCooldowns.contains(playerUUID)) {
-            event.setCancelled(true);
-            player.updateInventory();
-        } else {
-            placeCooldowns.add(playerUUID);
-            RegionScheduler regionScheduler = plugin.getServer().getRegionScheduler();
-            regionScheduler.runDelayed(plugin, player.getLocation(), handle -> placeCooldowns.remove(playerUUID), PLACE_DELAY_TICKS);
-        }
+    @Override
+    public void onEnable() {
+        Bukkit.getPluginManager().registerEvents(this, this);
     }
 
     @EventHandler
-    public void onEntityDamageByEntity(@NotNull EntityDamageByEntityEvent event) {
-        if (!(event.getEntity() instanceof EnderCrystal crystal)) return;
-        if (!(event.getDamager() instanceof Player)) return;
+    public void onCrystalBreak(BlockBreakEvent event) {
+        Block block = event.getBlock();
 
-        event.setCancelled(true);
+        if (block.getType() == Material.END_CRYSTAL) {
+            EnderCrystal crystal = (EnderCrystal) block.getWorld().spawn(block.getLocation(), EnderCrystal.class);
+            UUID crystalUUID = crystal.getUniqueId();
+            int currentTick = Bukkit.getCurrentTick();
 
-        final Location loc = crystal.getLocation();
-        final World world = loc.getWorld();
-        if (world == null) return;
+            if (crystalSpawnTicks.containsKey(crystalUUID)) {
+                int spawnTick = crystalSpawnTicks.get(crystalUUID);
+                int ageInTicks = currentTick - spawnTick;
 
-        RegionScheduler regionScheduler = plugin.getServer().getRegionScheduler();
-        regionScheduler.runDelayed(plugin, loc, handle -> {
-            if (!crystal.isDead()) {
-                crystal.remove();
-                world.createExplosion(loc, EXPLOSION_POWER, false, true);
+                if (ageInTicks < 4) {
+                    event.setCancelled(true);
+                }
             }
-        }, EXPLOSION_DELAY_TICKS);
+        }
+    }
+
+    public void trackCrystal(EnderCrystal crystal) {
+        UUID crystalUUID = crystal.getUniqueId();
+        int spawnTick = Bukkit.getCurrentTick();
+        crystalSpawnTicks.put(crystalUUID, spawnTick);
+
+        new BukkitRunnable() {
+            @Override
+            public void run() {
+                crystalSpawnTicks.remove(crystalUUID);
+            }
+        }.runTaskLater(this, 6000L);
     }
 }
